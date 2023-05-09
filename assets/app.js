188a26ab00e4555be6b5ca18c77272763a2740a9
hybridx <hybridx18@gmail.com>
2023-05-10 01:35:03 +0530

    first commit

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+// Include single point of entry for CSS
+import "./scss/style.scss";
+
+// Include any javascript modules here
+// import "./javascript/module";
---
+++ assets/build/app.js
@@ -0,0 +1,38 @@
+"use strict";
+(self["webpackChunkmg_styling_assessment"] = self["webpackChunkmg_styling_assessment"] || []).push([["app"],{
+
+/***/ "./assets/app.js":
+/*!***********************!*\
+  !*** ./assets/app.js ***!
+  \***********************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/style.scss */ "./assets/scss/style.scss");
+// Include single point of entry for CSS
+
+
+// Include any javascript modules here
+// import "./javascript/module";
+
+/***/ }),
+
+/***/ "./assets/scss/style.scss":
+/*!********************************!*\
+  !*** ./assets/scss/style.scss ***!
+  \********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/app.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBQUE7QUFDMkI7O0FBRTNCO0FBQ0E7Ozs7Ozs7Ozs7O0FDSkEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9tZy1zdHlsaW5nLWFzc2Vzc21lbnQvLi9hc3NldHMvYXBwLmpzIiwid2VicGFjazovL21nLXN0eWxpbmctYXNzZXNzbWVudC8uL2Fzc2V0cy9zY3NzL3N0eWxlLnNjc3MiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gSW5jbHVkZSBzaW5nbGUgcG9pbnQgb2YgZW50cnkgZm9yIENTU1xuaW1wb3J0IFwiLi9zY3NzL3N0eWxlLnNjc3NcIjtcblxuLy8gSW5jbHVkZSBhbnkgamF2YXNjcmlwdCBtb2R1bGVzIGhlcmVcbi8vIGltcG9ydCBcIi4vamF2YXNjcmlwdC9tb2R1bGVcIjsiLCIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
